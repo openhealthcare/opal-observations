0ca1d8108e6ffc1ac409fbb9563bcfb3263d594c
David Miller <david@deadpansincerity.com>
2014-12-20 22:44:23 +0000

    Force non-null on episode subrecords

---
+++ obs/migrations/0005_auto__chg_field_observation_episode.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Observation.episode'
+        db.alter_column(u'obs_observation', 'episode_id', self.gf('django.db.models.fields.related.ForeignKey')(default=-1, to=orm['opal.Episode']))
+
+    def backwards(self, orm):
+
+        # Changing field 'Observation.episode'
+        db.alter_column(u'obs_observation', 'episode_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['opal.Episode'], null=True))
+
+    models = {
+        u'obs.observation': {
+            'Meta': {'object_name': 'Observation'},
+            'bp_diastolic': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'bp_systolic': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'consistency_token': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
+            'date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'episode': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['opal.Episode']"}),
+            'height': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pulse': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'resp_rate': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'sp02': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'temperature': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'time': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'weight': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'opal.episode': {
+            'Meta': {'object_name': 'Episode'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'consistency_token': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
+            'date_of_admission': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'discharge_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'patient': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['opal.Patient']"})
+        },
+        u'opal.patient': {
+            'Meta': {'object_name': 'Patient'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        }
+    }
+
+    complete_apps = ['obs']
