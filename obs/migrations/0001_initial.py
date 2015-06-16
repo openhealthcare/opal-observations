18d9e75826209d99b2365967a2146d964c89b4a6
David Miller <david@deadpansincerity.com>
2015-06-16 13:08:36 +0100

    Update to Django 1.7

--- obs/migrations/0001_initial.py
+++ obs/migrations/0001_initial.py
@@ -1,63 +1,34 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import opal.models.mixins
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Observation'
-        db.create_table(u'obs_observation', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('consistency_token', self.gf('django.db.models.fields.CharField')(max_length=8)),
-            ('episode', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['opal.Episode'], null=True)),
-            ('bp_systolic', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
-            ('bp_diastolic', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
-            ('pulse', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
-            ('resp_rate', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
-            ('sp02', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
-            ('temperature', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
-            ('height', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
-            ('weight', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
-        ))
-        db.send_create_signal(u'obs', ['Observation'])
+    dependencies = [
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Observation'
-        db.delete_table(u'obs_observation')
-
-
-    models = {
-        u'obs.observation': {
-            'Meta': {'object_name': 'Observation'},
-            'bp_diastolic': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'bp_systolic': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'consistency_token': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
-            'episode': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['opal.Episode']", 'null': 'True'}),
-            'height': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'pulse': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'resp_rate': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'sp02': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'temperature': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'weight': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
-        },
-        u'opal.episode': {
-            'Meta': {'object_name': 'Episode'},
-            'active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'consistency_token': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
-            'date_of_admission': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
-            'discharge_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'patient': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['opal.Patient']"})
-        },
-        u'opal.patient': {
-            'Meta': {'object_name': 'Patient'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
-        }
-    }
-
-    complete_apps = ['obs']
+    operations = [
+        migrations.CreateModel(
+            name='Observation',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('consistency_token', models.CharField(max_length=8)),
+                ('bp_systolic', models.FloatField(null=True, blank=True)),
+                ('bp_diastolic', models.FloatField(null=True, blank=True)),
+                ('pulse', models.FloatField(null=True, blank=True)),
+                ('resp_rate', models.FloatField(null=True, blank=True)),
+                ('sp02', models.FloatField(null=True, blank=True)),
+                ('temperature', models.FloatField(null=True, blank=True)),
+                ('height', models.FloatField(null=True, blank=True)),
+                ('weight', models.FloatField(null=True, blank=True)),
+                ('datetime', models.DateTimeField(null=True, blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(opal.models.mixins.UpdatesFromDictMixin, models.Model),
+        ),
+    ]
---
+++ obs/migrations/0002_observation_episode.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('opal', '0001_initial'),
+        ('obs', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='observation',
+            name='episode',
+            field=models.ForeignKey(to='opal.Episode'),
+            preserve_default=True,
+        ),
+    ]
