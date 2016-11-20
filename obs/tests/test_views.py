69e1d0f21c5e3d9044ea98dca83c3e366cb827d5
David Miller <david@deadpansincerity.com>
2016-11-20 22:07:41 +0000

    Add test for obs chart view

---
+++ obs/tests/test_views.py
@@ -0,0 +1,14 @@
+"""
+Unittests for obs.views
+"""
+from opal.core.test import OpalTestCase
+from mock import MagicMock
+
+from obs import views
+
+class ObsChartViewTestCase(OpalTestCase):
+    def test_get_context_data(self):
+        self.patient, self.episode = self.new_patient_and_episode_please()
+        view = views.ObsChartView()
+        ctx = view.get_context_data(episode_id=self.episode.id)
+        self.assertEqual(ctx['episode'].id, self.episode.id)
