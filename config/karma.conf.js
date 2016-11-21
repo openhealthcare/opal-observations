8035b04637603dbe0f39d17d6e697afd9a8df611
fredkingham <fredkingham@gmail.com>
2016-11-21 19:55:58 +0000

    added karma dependencies

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -3,6 +3,7 @@ module.exports = function(config){
     var preprocessors = {}
 
     preprocessors[__dirname+'/../obs/static/js/obs/**/*.js'] = 'coverage';
+    var additionalDependencies;
 
     if(process.env.TRAVIS){
         browsers = ["Firefox"];
@@ -11,6 +12,7 @@ module.exports = function(config){
             type: 'lcovonly', // lcov or lcovonly are required for generating lcov.info files
             dir: __dirname + '/../coverage/',
         };
+        additionalDependencies = require('/home/travis/virtualenv/python2.7/src/opal/config/karma_dependencies.js');
     }
     else{
         browsers = ['PhantomJS'];
@@ -19,55 +21,20 @@ module.exports = function(config){
             type : 'html',
             dir : __dirname + '/../htmlcov/js/'
         }
+        additionalDependencies = require('../../opal/config/karma_dependencies.js');
     }
 
     config.set({
         frameworks: ['jasmine'],
         browsers: browsers,
         basePath:  basePath,
-
-        files: [
-            //JASMINE,
-            //JASMINE_ADAPTER,
-
-            "lib/d3/d3.js",
-            "lib/c3-0.4.10/c3.js",
-
-            "lib/bower_components/angular/angular.js",
-            "lib/bower_components/angular-route/angular-route.js",
-            "lib/bower_components/angular-resource/angular-resource.js",
-            "lib/bower_components/angular-cookies/angular-cookies.js",
-            "lib/bower_components/angular-mocks/angular-mocks.js",
-
-            'lib/angular-ui-utils-0.1.0/ui-utils.js',
-            "lib/ui-bootstrap-tpls-0.14.3.js",
-            "lib/angulartics-0.17.2/angulartics.min.js",
-            "lib/angulartics-0.17.2/angulartics-ga.min.js",
-            'lib/ngprogress-lite/ngprogress-lite.js',
-            'lib/jquery-1.11.3/jquery-1.11.3.js',
-            'lib/utils/underscore.js',
-            'lib/utils/showdown.js',
-            'lib/utils/moment.js',
-            'lib/bower_components/angular-growl-v2/build/angular-growl.js',
-            'lib/bower_components/ment.io/dist/mentio.js',
-            'lib/bower_components/ment.io/dist/templates.js',
-            'lib/bower_components/angular-ui-select/dist/select.js',
-            "lib/bower_components/angular-local-storage/dist/angular-local-storage.js",
-
-            'opal/utils.js',
-            'opal/opaldown.js',
-            'opal/directives.js',
-            'opal/filters.js',
-            'opal/services_module.js',
-            'opal/services/*.js',
-            'opal/services/flow.js',
-            'opal/controllers_module.js',
-            'opal/controllers/*.js',
-
-             __dirname+'/../obs/static/js/obs/controllers/*.js',
-             __dirname+'/../obs/static/js/obs/services/**/*.js',
-             __dirname+'/../obs/static/js/test/*.js',
-        ],
+        files: additionalDependencies().concat([
+          "lib/d3/d3.js",
+          "lib/c3-0.4.10/c3.js",
+          __dirname+'/../obs/static/js/obs/controllers/*.js',
+          __dirname+'/../obs/static/js/obs/services/**/*.js',
+          __dirname+'/../obs/static/js/test/*.js',
+        ]),
 
         // Stolen from http://oligofren.wordpress.com/2014/05/27/running-karma-tests-on-browserstack/
         browserDisconnectTimeout : 10000, // default 2000
