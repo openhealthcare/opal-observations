67dfdcb538a51921aa4579a1c59e6f37bda0e39d
fredkingham <fredkingham@gmail.com>
2017-06-03 13:44:34 +0100

    add a v0.4.0

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,14 +1,6 @@
 module.exports = function(config){
-  var opalPath;
-  var projectName = "lab";
-  if(process.env.TRAVIS){
-    python_version = process.env.TRAVIS_PYTHON_VERSION;
-    opalPath = '/home/travis/virtualenv/python' + python_version + '/src/opal';
-  }
-  else{
-    opalPath = '../../opal';
-  }
-  var karmaDefaults = require(opalPath + '/config/karma_defaults.js');
+  var opalPath = process.env.OPAL_LOCATION;
+  var karmaDefaults = require(opalPath + '/opal/tests/js_config/karma_defaults.js');
   var baseDir = __dirname + '/..';
   var coverageFiles = [
     __dirname+'/../obs/static/js/obs/**/*.js'
