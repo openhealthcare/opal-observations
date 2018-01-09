9c178837e87333c09e8dbcfc91c9693b000f3ce1
David Miller <david@deadpansincerity.com>
2018-01-09 09:45:27 +0000

    Don't define ObservationsPlugin twice

--- obs/__init__.py
+++ obs/__init__.py
@@ -1,18 +1,3 @@
 """
 Package definition
 """
-from opal.core import plugins
-
-from obs import urls
-
-class ObservationsPlugin(plugins.OpalPlugin):
-    """
-    Plugin for obs
-    """
-    urls = urls.urlpatterns
-    javascripts = {
-        'opal.controllers': [
-            'js/obs/controllers/obs_chart.js',
-            'js/obs/services/records/observation.js',
-        ]
-    }
